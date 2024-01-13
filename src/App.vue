f41f007ccfda4fa7dd181c499287918c49b80e4c
KarinaVilla01 <104106557+KarinaVilla01@users.noreply.github.com>
2024-01-13 15:40:51 -0700

    first

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  function openCart(){
+
+  }
+</script>
+
+<template>
+  <main>
+    <div class="cart-price">
+      <h6>Shopping Cart</h6>
+      <ul class="listCart">
+      </ul>
+      <div class="checkOut">
+        <div class="total">$0</div>
+        <div class="closeShopping">Close</div>
+      </div>
+    </div>
+    <div id="button-create">
+      <button id="buy" @click="openCart">Comprar</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
