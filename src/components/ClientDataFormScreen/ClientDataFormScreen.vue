3920a4f810496f95487004a815cbf23e82b6e1d4
Karina <fil.01.21@gmail.com>
2024-03-05 13:57:16 -0700

    Cuestionario bonito

--- src/components/ClientDataFormScreen/ClientDataFormScreen.vue
+++ src/components/ClientDataFormScreen/ClientDataFormScreen.vue
@@ -1,55 +1,115 @@
 <script setup>
-import {ref, watch} from "vue";
+import { ref, watch } from "vue";
 
-const clientData=defineModel({default:{}})
-const paymentType=ref( 0)
-const location=ref(35090)
-const firstName=ref("")
-const lastName=ref("")
-const email=ref("")
-const phone=ref("")
+const clientData = defineModel({ default: {} })
+const paymentType = ref(0)
+const location = ref(35090)
+const firstName = ref("")
+const lastName = ref("")
+const email = ref("")
+const phone = ref("")
 
-watch(paymentType, ()=>{
-  clientData.value={...clientData.value, paymentType}
+// Mensajes de error
+const firstNameError = ref('');
+const lastNameError = ref('');
+const emailError = ref('');
+const phoneError = ref('');
+
+const validateEmail = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+watch(firstName, (newValue) => {
+  firstNameError.value = newValue ? '' : 'El nombre es requerido';
+});
+
+watch(lastName, (newValue) => {
+  lastNameError.value = newValue ? '' : 'El apellido es requerido';
+});
+
+watch(email, (newValue) => {
+  emailError.value = validateEmail(newValue) ? '' : 'El correo no es válido';
+});
+
+watch(phone, (newValue) => {
+  const regex = /^\d{10}$/;
+  phoneError.value = regex.test(newValue) ? '' : 'El teléfono debe tener 10 dígitos';
+});
+
+watch(paymentType, () => {
+  clientData.value = { ...clientData.value, paymentType }
 })
-watch(location, ()=>{
-  clientData.value={...clientData.value, location}
+watch(location, () => {
+  clientData.value = { ...clientData.value, location }
 })
-watch(firstName, ()=>{
-  clientData.value={...clientData.value, firsNme: firstName}
+watch(firstName, () => {
+  clientData.value = { ...clientData.value, firsNme: firstName }
 })
-watch(lastName, ()=>{
-  clientData.value={...clientData.value, lastName}
+watch(lastName, () => {
+  clientData.value = { ...clientData.value, lastName }
 })
-watch(email, ()=>{
-  clientData.value={...clientData.value, email}
+watch(email, () => {
+  clientData.value = { ...clientData.value, email }
 })
-watch(phone, ()=>{
-  clientData.value={...clientData.value, phone}
+watch(phone, () => {
+  clientData.value = { ...clientData.value, phone }
 })
 </script>
 
 <template>
-<div>
-  <select name="paymentType" v-model="paymentType">
-    <option value=0>Visa/MasterCard</option>
-    <option value=2>Amex</option>
-  </select><br/>
-  <select name="location" v-model="location">
-    <option value=35090>Hermosillo/Quiroga</option>
-    <option value=35145>Mexicali</option>
-  </select><br/>
-  <label>Nombre</label>
-  <input v-model="firstName"/><br/>
-  <label>Apellido</label>
-  <input v-model="lastName"/><br/>
-  <label>Correo</label>
-  <input v-model="email"/><br/>
-  <label>Telefono</label>
-  <input v-model="phone"/>
-</div>
+  <div class="card-form__inner">
+    <div class="card-input">
+      <h2>Datos de pago:</h2>
+      <label for="cardMonth" class="card-input__label">Tipo de tarjeta</label>
+      <div class="icon-input-center">
+        <i class="fa-regular fa-credit-card"></i>
+        <select name="paymentType" v-model="paymentType" class="card-input__input -select">
+          <option value=0>Visa/MasterCard</option>
+          <option value=2>Amex</option>
+        </select>
+      </div>
+      <label for="cardMonth" class="card-input__label">Sucursal de compra</label>
+      <div class="icon-input-center">
+        <i class="fa-solid fa-location-dot"></i>
+        <select name="location" v-model="location" class="card-input__input -select">
+          <option value=35090>Hermosillo/Quiroga</option>
+          <option value=35145>Mexicali</option>
+        </select>
+      </div>
+      <label class="card-input__label">Nombre</label>
+      <div class="input-container">
+        <i class="fa fa-user"></i>
+        <input v-model="firstName" type="text" class="card-input__input" placeholder="Eduardo de Jesús"
+          @blur="validateFirstName">
+        </div>
+        <div v-if="firstNameError" class="error">{{ firstNameError }}</div>
+      <label class="card-input__label">Apellido</label>
+      <div class="input-container">
+        <i class="fa-regular fa-user"></i>
+        <input v-model="lastName" type="text" class="card-input__input" placeholder="López Martínez"
+          @blur="validateLastName">
+        </div>
+        <div v-if="lastNameError" class="error">{{ lastNameError }}</div>
+      <label class="card-input__label">Correo</label>
+      <div class="input-container">
+        <i class="fa-solid fa-envelope"></i>
+        <input v-model="email" type="email" class="card-input__input" placeholder="email@carreracarwash.com"
+          @blur="validateEmail">
+        </div>
+        <div v-if="emailError" class="error">{{ emailError }}</div>
+      <label class="card-input__label">Telefono</label>
+      <div class="input-container">
+        <i class="fa-solid fa-phone"></i>
+        <input v-model="phone" type="text" maxlength="10" class="card-input__input" placeholder="55 123456789"
+          @blur="validatePhone">
+        </div>
+        <div v-if="phoneError" class="error">{{ phoneError }}</div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
-</style>
+.error {
+  color: red;
+  font-size: 0.7em;
+}</style>
